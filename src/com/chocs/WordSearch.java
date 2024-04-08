519abadfaeb5aa49ae2e9f6e48c766cec0db322f
chocs <fernandobhirsch@gmail.com>
2024-04-08 13:18:02 -0300

    Reduces if and try-catch nests

--- src/com/chocs/WordSearch.java
+++ src/com/chocs/WordSearch.java
@@ -1,6 +1,7 @@
 package com.chocs;
 
 import java.io.*;
+import java.util.Arrays;
 
 public class WordSearch {
     public static void main(String[] args) {
@@ -10,36 +11,26 @@ public class WordSearch {
         String search = "a";
         String line = "";
 
-        if(fileArray != null) {
-            for (int i = 0; i < fileArray.length; i++) {
-                int countLine = 0;
-
-                try {
-                    BufferedReader br = new BufferedReader(new FileReader(fileArray[i]));
-
-                    try {
-                        while ((line = br.readLine()) != null) {
-                            String[] words = line.split(" ");
-                            countLine++;
-
-                            for (String word : words) {
-                                if (word.equals(search)) {
-                                    System.out.println(fileArray[i].getName() + " contains " + word + " at line " + countLine);
-                                }
-                            }
-                        }
-                    } catch (IOException e) {
-                        System.out.println("Error reading file");
-                        e.printStackTrace();
-                    }
-                } catch (FileNotFoundException e) {
-                    System.out.println("File not found");
-                    throw new RuntimeException(e);
-                }
-            }
-        } else {
+        if (fileArray == null) {
             System.out.println("There are no files in folder");
+            System.exit(69);
         }
 
+        for (File file : fileArray) {
+            System.out.println();
+
+            try (FileReader fr = new FileReader(file);
+                 BufferedReader br = new BufferedReader(fr))
+            {
+                for (int countLine = 1; (line = br.readLine()) != null; countLine += 1)
+                {
+                    if(Arrays.asList(line.split(" ")).contains(search)) {
+                        System.out.println(file.getName() + " contains '" + search + "' at line " + countLine);
+                    }
+                }
+            } catch (IOException e) {
+                System.out.println(e.getMessage());
+            }
+        }
     }
 }
